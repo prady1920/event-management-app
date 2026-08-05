290b953168ffe83e316a203b69f58170c6a03379
prady1920 <prady1920@gmail.com>
2026-08-05 12:11:43 +0530

    Change store export to default export

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import eventsReducer from './eventsSlice'
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     events: eventsReducer
   }
 })
+
+export default store
